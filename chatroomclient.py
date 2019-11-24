bb852431636522ec62fc750a96f372aaa90d1429
Karl Vincent M. Espiritu <55738965+espiritukarl@users.noreply.github.com>
2019-11-24 22:20:13 +0800

    Update chatroomclient.py

--- chatroomclient.py
+++ chatroomclient.py
@@ -1,11 +1,10 @@
-# Version 1.00
+# Version 1.10 from https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
 
-
 def receive():
-    """Handles receiving of messages."""
+    # Handles receiving of messages
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
@@ -14,30 +13,29 @@ def receive():
             break
 
 
-def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
+def send(event=None):  # event is for tkinter
+    # handles sending of messages
     msg = my_msg.get()
     my_msg.set("")  # Clears input field.
     client_socket.send(bytes(msg, "utf8"))
     if msg == "{quit}":
         client_socket.close()
-        top.quit()
+        top.destroy()
 
 
 def on_closing(event=None):
-    """This function is to be called when the window is closed."""
+    # for closing of window
     my_msg.set("{quit}")
     send()
 
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("Chatbox")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
 scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+msg_list = tkinter.Listbox(messages_frame, height=35, width=75, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
@@ -51,13 +49,9 @@ send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the sockets part----
+# sockets:
 HOST = input('Enter host: ')
-PORT = input('Enter port: ')
-if not PORT:
-    PORT = 33000
-else:
-    PORT = int(PORT)
+PORT = 33000
 
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
