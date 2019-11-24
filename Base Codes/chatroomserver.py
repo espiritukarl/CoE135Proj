e225b37ea90e51f452b490ec81883b92fa8d0e75
Karl Vincent M. Espiritu <55738965+espiritukarl@users.noreply.github.com>
2019-11-25 03:16:40 +0800

    Update chatroomserver.py

--- Base Codes/chatroomserver.py
+++ Base Codes/chatroomserver.py
@@ -1,7 +1,9 @@
-# Version 1.30
-# Final. Just need to integrate to Video-Audio stream. No more client w/ server
+# Version 1.20
+# Rolled back from Version 1.30
+
 from socket import AF_INET, socket, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
+import tkinter
 
 def accept_connections():
     # accepts incoming clients
@@ -17,7 +19,7 @@ def handler(client):
     name = client.recv(BUFSIZ).decode("utf8")
     welcome = 'Welcome to the chatroom %s! Type {quit} if you want to exit.' % name
     if not name:
-        client.close()
+        return 
     client.send(bytes(welcome, "utf8"))
     msg = "%s has joined the chat!" % name
     broadcast(bytes(msg, "utf8"))
@@ -40,12 +42,56 @@ def broadcast(msg, prefix=""):  # prefix is for name identification.
     # show message to clients
     for sock in clients:
         sock.send(bytes(prefix, "utf8")+msg)
-        
+
+def receive():
+    # Handles receiving of messages
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:  # Possibly client has left the chat.
+            break
+
+def send(event=None):  # event is for tkinter
+    # handles sending of messages
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        top.destroy()
+
+def on_closing(event=None):
+    # for closing of window
+    my_msg.set("{quit}")
+    send()
+
+top = tkinter.Tk()
+top.title("Chatbox")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = tkinter.Listbox(messages_frame, height=35, width=75, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
 # sockets
 clients = {}
 addresses = {}
 
-HOST = ''
+HOST = '127.0.0.1'
 PORT = 33000
 
 BUFSIZ = 1024
@@ -61,5 +107,15 @@ if __name__ == "__main__":
     print("Waiting for connection...")
     ACCEPT_THREAD = Thread(target=accept_connections)
     ACCEPT_THREAD.start()
+    
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    client_socket.connect(ADDR)
+    
+    receive_thread = Thread(target=receive)
+    receive_thread.start()
+    
+    tkinter.mainloop()
+    
     ACCEPT_THREAD.join()
     SERVER.close()
